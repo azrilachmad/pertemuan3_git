b9bcc3a6fb2df9d3d84aff040efc29d41090a633
azrilachmad <azrilachmad4@gmail.com>
2022-07-06 10:07:24 +0700

    Commit Tugas Pertemuan 3 : Program Perkenalan

---
+++ perkenalan.js
@@ -0,0 +1,9 @@
+const introduction = {
+    name : "Azril Achmad",
+    bornDate: "20 December 2000",
+    city: "Bandung",
+    age : 21,
+}
+
+
+console.log(`Hello, my name is ${introduction.name}, I am ${introduction.age} years old, i was born on ${introduction.bornDate} in ${introduction.city}.`)
